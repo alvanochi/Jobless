2cda36efabe8c5c7e7f029a19bae01a5626e1ba3
alvanochi <alvanochi@gmail.com>
2023-01-31 19:48:49 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        donker : '#001AFF'
+      },
+      fontFamily : {
+        poppins : ['poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
